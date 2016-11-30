d36b9a6fc0b72cca08ef0720d060d06fbf7b694f
Law-God <767030396@qq.com>
2016-11-30 17:51:47 +0800

    ''

---
+++ index.js
@@ -0,0 +1,4 @@
+﻿require("./bundle.less");
+
+let React = require("react");
+let ReactDOM = require("react-dom");
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+﻿var webpack = require("webpack");
+
+
+
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+	
+	entry : ['./index.js'],
+
+	output : {
+		path : "./dist",
+		filename : "bundle.js"
+	},
+
+	module : {
+		loaders : [
+			{
+				test : /\.js$/,
+				loaders : ["babel"],
+				exclude : /node_modules/
+			},
+            {
+			    test: /\.css$/,
+			     loader:  ExtractTextPlugin.extract("style-loader","css-loader")
+			},
+			,
+            {
+	            test:  /\.scss$/,
+	             loader:  "style!css!sass"
+	        },
+	            {
+	            test:  /\.less$/,
+	             loader:  "style!css!less"
+	        },
+		]
+	},
+
+	plugins: [
+    	new ExtractTextPlugin("styles.css")
+	]
+}
