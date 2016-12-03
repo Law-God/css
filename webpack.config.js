ad012c426ac3253093ff3ada222f39c3c8d0e845
Law-God <767030396@qq.com>
2016-12-03 10:09:16 +0800

    ''

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,20 @@
 ﻿var webpack = require("webpack");
-
-
-
+var path = require("path");
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var WebpackMd5Hash = require("webpack-md5-hash");
 
 module.exports = {
 	
-	entry : ['./index.js'],
+	entry :  {
+		bundle : "./index.js",
+		lib : ["react","react-dom"]
+	},
 
 	output : {
 		path : "./dist",
-		filename : "bundle.js"
+		filename : "[name][chunkhash:8].js",
+		//将entry中lib单独打包一个lib.js中
+		chunkFilename : "[name][chunkhash:8].js"
 	},
 
 	module : {
@@ -21,22 +25,21 @@ module.exports = {
 				exclude : /node_modules/
 			},
             {
-			    test: /\.css$/,
-			     loader:  ExtractTextPlugin.extract("style-loader","css-loader")
-			},
-			,
-            {
-	            test:  /\.scss$/,
-	             loader:  "style!css!sass"
-	        },
-	            {
-	            test:  /\.less$/,
-	             loader:  "style!css!less"
-	        },
+			    test: /\.(less|css)$/,
+			    loader:  ExtractTextPlugin.extract("style-loader","css-loader!less-loader")
+			}
 		]
 	},
-
+	resolve : {
+		alias : {
+			bundlecss : path.join(__dirname,"./src/css/bundle.less")
+		}
+	},
 	plugins: [
-    	new ExtractTextPlugin("styles.css")
+    	new ExtractTextPlugin("[name][contenthash:8].css"),
+    	new webpack.optimize.CommonsChunkPlugin({
+    		names : ["lib"]
+    	}),
+    	new WebpackMd5Hash()
 	]
 }
